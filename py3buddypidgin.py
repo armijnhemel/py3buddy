9d3fdfd369327e288f4c7e4e814faa048939627d
Armijn Hemel <armijn@tjaldur.nl>
2018-10-11 22:03:44 +0200

    PEP8 changes

--- py3buddypidgin.py
+++ py3buddypidgin.py
@@ -1,101 +1,113 @@
 #!/usr/bin/python3
 
-## Demo program to show how to use the py3buddy module with DBus and integrate
-## with Pidgin
-##
-## Uses python3-gobject-base and python3-pydbus (package names from Fedora)
-##
-## Copyright 2017-2018 - Armijn Hemel for Tjaldur Software Governance Solutions
-## SPDX-Identifier: GPL-3.0
+# Demo program to show how to use the py3buddy module with DBus and integrate
+# with Pidgin
+#
+# Uses python3-gobject-base and python3-pydbus (package names from Fedora)
+#
+# Copyright 2017-2018 - Armijn Hemel for Tjaldur Software Governance Solutions
+# SPDX-Identifier: GPL-3.0
 
-import sys, os, argparse, configparser, random, time, calendar, re
+import sys
+import os
+import argparse
+import configparser
+import random
+import time
+import calendar
+import re
 import py3buddy
-import pydbus, gi
+import pydbus
+import gi
+
 
 def processmsg(account, sender, message, conversation, flags):
-	## a demo version to show some of the capabilities of
-	## the iBuddy
+    # a demo version to show some of the capabilities of
+    # the iBuddy
 
-	## first reset the iBuddy
-	ibuddy.reset()
+    # first reset the iBuddy
+    ibuddy.reset()
 
-	## a list of smileys as sent by Google Hangout
-	smileys = [':D', ':-D', '^_^', ':-)', ':)', '☺️'] 
+    # a list of smileys as sent by Google Hangout
+    smileys = [':D', ':-D', '^_^', ':-)', ':)', '☺️']
 
-	## a command to execute when a smiley is received
-	smilecommand = 'RED:HEART:WINGSHIGH:GO:SHORTSLEEP:YELLOW:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:BLUE:WINGSHIGH:GO:SHORTSLEEP:PURPLE:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:CYAN:WINGSHIGH:GO:SHORTSLEEP:WHITE:NOHEART:WINGSLOW:GO:SHORTSLEEP:RESET'
-	for s in smileys:
-		if s in message:
-			ibuddy.executecommand(smilecommand)
-			break
+    # a command to execute when a smiley is received
+    smilecommand = 'RED:HEART:WINGSHIGH:GO:SHORTSLEEP:YELLOW:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:BLUE:WINGSHIGH:GO:SHORTSLEEP:PURPLE:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:CYAN:WINGSHIGH:GO:SHORTSLEEP:WHITE:NOHEART:WINGSLOW:GO:SHORTSLEEP:RESET'
+    for s in smileys:
+        if s in message:
+            ibuddy.executecommand(smilecommand)
+            break
 
-	ibuddy.reset()
+    ibuddy.reset()
+
 
 def main(argv):
-	parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
 
-	## options for the commandline
-	parser.add_argument("-c", "--config", action="store", dest="cfg", help="path to configuration file", metavar="FILE")
-	args = parser.parse_args()
+    # options for the commandline
+    parser.add_argument("-c", "--config", action="store", dest="cfg",
+                        help="path to configuration file", metavar="FILE")
+    args = parser.parse_args()
 
-	## first some sanity checks for the configuration file
-	if args.cfg == None:
-		parser.error("Configuration file missing")
+    # first some sanity checks for the configuration file
+    if args.cfg is None:
+        parser.error("Configuration file missing")
 
-	if not os.path.exists(args.cfg):
-		parser.error("Configuration file does not exist")
+    if not os.path.exists(args.cfg):
+        parser.error("Configuration file does not exist")
 
-	## then parse the configuration file
-	config = configparser.ConfigParser()
+    # then parse the configuration file
+    config = configparser.ConfigParser()
 
-	configfile = open(args.cfg, 'r')
+    configfile = open(args.cfg, 'r')
 
-	try:
-		config.readfp(configfile)
-	except Exception:
-		print("Cannot read configuration file", file=sys.stderr)
-		sys.exit(1)
+    try:
+        config.readfp(configfile)
+    except Exception:
+        print("Cannot read configuration file", file=sys.stderr)
+        sys.exit(1)
 
-	buddy_config = {}
-	for section in config.sections():
-		if section == 'ibuddy':
-			try:
-				productid = int(config.get(section, 'productid'))
-				buddy_config['productid'] = productid
-			except:
-				pass
+    buddy_config = {}
+    for section in config.sections():
+        if section == 'ibuddy':
+            try:
+                productid = int(config.get(section, 'productid'))
+                buddy_config['productid'] = productid
+            except:
+                pass
 
-			buddy_config['reset_position'] = False
-			try:
-				reset_position_val = config.get(section, 'reset_position')
-				if reset_position_val == 'yes':
-					buddy_config['reset_position'] = True
-			except:
-				pass
-		if section == 'twitter':
-			pass
+            buddy_config['reset_position'] = False
+            try:
+                reset_position_val = config.get(section, 'reset_position')
+                if reset_position_val == 'yes':
+                    buddy_config['reset_position'] = True
+            except:
+                pass
+        if section == 'twitter':
+            pass
 
-	## This is very ugly, but the only way (I know) to expose the iBuddy to the
-	## method processing the message from Pidgin
-	global ibuddy
+    # This is very ugly, but the only way (I know) to expose the iBuddy to the
+    # method processing the message from Pidgin
+    global ibuddy
 
-	## initialize an iBuddy and check if a device was found and is accessible
-	ibuddy = py3buddy.iBuddy(buddy_config)
-	if ibuddy.dev == None:
-		print("No iBuddy found, or iBuddy not accessible", file=sys.stderr)
-		sys.exit(1)
+    # initialize an iBuddy and check if a device was found and is accessible
+    ibuddy = py3buddy.iBuddy(buddy_config)
+    if ibuddy.dev is None:
+        print("No iBuddy found, or iBuddy not accessible", file=sys.stderr)
+        sys.exit(1)
 
-	## connect to session DBus
-	bus = pydbus.SessionBus()
+    # connect to session DBus
+    bus = pydbus.SessionBus()
 
-	## register a callback for messages that are received
-	purple = bus.get("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject")
-	purple.ReceivedImMsg.connect(processmsg)
+    # register a callback for messages that are received
+    purple = bus.get("im.pidgin.purple.PurpleService",
+                     "/im/pidgin/purple/PurpleObject")
+    purple.ReceivedImMsg.connect(processmsg)
 
-	gi.repository.GObject.MainLoop().run()
+    gi.repository.GObject.MainLoop().run()
 
-	## finally reset the i-buddy again
-	ibuddy.reset()
+    # finally reset the i-buddy again
+    ibuddy.reset()
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
--- py3buddypidgindbus.py
+++ py3buddypidgindbus.py
@@ -1,64 +1,71 @@
 #!/usr/bin/python3
 
-## Demo program to show how to use the py3buddy module with DBus and integrate
-## with Pidgin
-##
-## Uses python3-gobject-base and python3-pydbus (package names from Fedora)
-##
-## Copyright 2017-2018 - Armijn Hemel for Tjaldur Software Governance Solutions
-## SPDX-Identifier: GPL-3.0
+# Demo program to show how to use the py3buddy module with DBus and integrate
+# with Pidgin
+#
+# Uses python3-gobject-base and python3-pydbus (package names from Fedora)
+#
+# Copyright 2017-2018 - Armijn Hemel for Tjaldur Software Governance Solutions
+# SPDX-Identifier: GPL-3.0
 
-import sys, os, re
+import sys
+import os
+import re
 import py3buddy
-import pydbus, gi
+import pydbus
+import gi
+
 
 def processmsg(account, sender, message, conversation, flags):
-	## a demo version to show some of the capabilities of
-	## the iBuddy
+    # a demo version to show some of the capabilities of
+    # the iBuddy
 
-	## first reset the iBuddy
-	ibuddy.ExecuteBuddyCommand("RESET")
+    # first reset the iBuddy
+    ibuddy.ExecuteBuddyCommand("RESET")
 
-	## a list of smileys as sent by Google Hangout
-	smileys = [':D', ':-D', '^_^', ':-)', ':)', '☺️'] 
+    # a list of smileys as sent by Google Hangout
+    smileys = [':D', ':-D', '^_^', ':-)', ':)', '☺️']
 
-	## a command to execute when a smiley is received: loop through a few colours, show a heartbeet, flap wings
-	smilecommand = 'RED:HEART:WINGSHIGH:GO:SHORTSLEEP:YELLOW:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:BLUE:WINGSHIGH:GO:SHORTSLEEP:PURPLE:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:CYAN:WINGSHIGH:GO:SHORTSLEEP:WHITE:NOHEART:WINGSLOW:GO:SHORTSLEEP:RESET'
-	for s in smileys:
-		if s in message:
-			ibuddy.ExecuteBuddyCommand(smilecommand)
-			break
+    # a command to execute when a smiley is received: loop through a
+    # few colours, show a heartbeet, flap wings
+    smilecommand = 'RED:HEART:WINGSHIGH:GO:SHORTSLEEP:YELLOW:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:BLUE:WINGSHIGH:GO:SHORTSLEEP:PURPLE:NOHEART:WINGSLOW:GO:SHORTSLEEP:HEART:CYAN:WINGSHIGH:GO:SHORTSLEEP:WHITE:NOHEART:WINGSLOW:GO:SHORTSLEEP:RESET'
+    for s in smileys:
+        if s in message:
+            ibuddy.ExecuteBuddyCommand(smilecommand)
+            break
 
-	## send an extra reset, as sometimes the iBuddy doesn't respond
-	## to all commands.
-	ibuddy.ExecuteBuddyCommand("RESET")
+    # send an extra reset, as sometimes the iBuddy doesn't respond
+    # to all commands.
+    ibuddy.ExecuteBuddyCommand("RESET")
+
 
 def main(argv):
-	## connect to session DBus
-	bus = pydbus.SessionBus()
+    # connect to session DBus
+    bus = pydbus.SessionBus()
 
-	## This is very ugly, but the only way (I know) to expose the iBuddy to the
-	## method processing the message from Pidgin
-	global ibuddy
+    # This is very ugly, but the only way (I know) to expose the iBuddy to the
+    # method processing the message from Pidgin
+    global ibuddy
 
-	## register a callback for messages that are received
-	try:
-		ibuddy = bus.get("nl.tjaldur.IBuddy", "/nl/tjaldur/IBuddy")
-	except:
-		ibuddy = None
+    # register a callback for messages that are received
+    try:
+        ibuddy = bus.get("nl.tjaldur.IBuddy", "/nl/tjaldur/IBuddy")
+    except:
+        ibuddy = None
 
-	if ibuddy == None:
-		print("iBuddy not found, exiting", file=sys.stderr)
-		sys.exit(1)
+    if ibuddy is None:
+        print("iBuddy not found, exiting", file=sys.stderr)
+        sys.exit(1)
 
-	## register a callback for messages that are received
-	purple = bus.get("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject")
-	purple.ReceivedImMsg.connect(processmsg)
+    # register a callback for messages that are received
+    purple = bus.get("im.pidgin.purple.PurpleService",
+                     "/im/pidgin/purple/PurpleObject")
+    purple.ReceivedImMsg.connect(processmsg)
 
-	gi.repository.GObject.MainLoop().run()
+    gi.repository.GObject.MainLoop().run()
 
-	## finally reset the i-buddy again
-	ibuddy.ExecuteBuddyCommand("RESET")
+    # finally reset the i-buddy again
+    ibuddy.ExecuteBuddyCommand("RESET")
 
 if __name__ == "__main__":
-	main(sys.argv)
+    main(sys.argv)
